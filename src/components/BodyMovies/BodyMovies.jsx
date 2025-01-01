2cd5e5596947d11dbe136ca70bf4e8dbb3b5c718
Kirill Senchuk <kimerland.project@gmail.com>
2025-01-01 23:41:38 +0300

    work with card

--- src/components/BodyMovies/BodyMovies.jsx
+++ src/components/BodyMovies/BodyMovies.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import movies from "./dataMovies";
+import styles from "./BodyMovies.module.scss";
 
 const BodyMovies = () => {
+  const [selectedGenre, setSelectedGenre] = useState("All");
+  const [filtredMovies, setFiltredMovies] = useState([]);
+
+  useEffect(() => {
+    if (selectedGenre === "All") {
+      setFiltredMovies(movies);
+    } else {
+      setFiltredMovies(movies.filter((movie) => movie.genre === selectedGenre));
+      // a new array containing only those movies whose movie.genre === selected.genre
+    }
+  }, [selectedGenre]); // useEffect will be work every time when selectedGenre was change
+
   return (
-    <main>
-      <p>All Films (100)</p>
-      {/* use useState for 100 */}
+    <main className={styles.main}>
+      <p>All Films ({movies.length})</p>
+      <div>
+        <button>
+          Genre
+          <select onChange={(e) => setSelectedGenre(e.target.value)}>
+            <option value="All">All</option>
+            <option value="Action">Action</option>
+            <option value="Golden Globe Winner">Golden Globe Winner</option>
+            <option value="Fiction">Fiction</option>
+          </select>
+        </button>
+
+        {/* button sort by raiting */}
+        <button>Sort by IMDb</button>
+
+        {/* <img src="arrow_drop_down.svg" />  */}
+
+        {/* make poster for my card */}
+        {filtredMovies.map((movie) => (
+          <div key={movie.id}>
+            <p>
+              {movie.title} {movie.genre} {movie.rating} {movie.description}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
---
+++ src/components/BodyMovies/dataMovies.js
@@ -0,0 +1,61 @@
+const movies = [
+  {
+    id: Math.random(),
+    title: "RRR",
+    genre: "Action",
+    rating: 9.9,
+    description:
+      "It centers around two real-life Indian revolutionaries, Alluri Sitarama Raju (Charan) and Komaram Bheem (Rama Rao)",
+  },
+  {
+    id: Math.random(),
+    title: "Varisu",
+    genre: "Fiction",
+    rating: 8.8,
+    description:
+      "It centers around two real-life Indian revolutionaries, Alluri Sitarama Raju (Charan) and Komaram Bheem (Rama Rao)",
+  },
+  {
+    id: Math.random(),
+    title: "Avengers",
+    genre: "Action",
+    rating: 9.7,
+    description:
+      "It centers around two real-life Indian revolutionaries, Alluri Sitarama Raju (Charan) and Komaram Bheem (Rama Rao)",
+  },
+  {
+    id: Math.random(),
+    title: "Master",
+    genre: "Golden Globe Winner",
+    year: 2000,
+    rating: 9.1,
+    description:
+      "It centers around two real-life Indian revolutionaries, Alluri Sitarama Raju (Charan) and Komaram Bheem (Rama Rao)",
+  },
+  {
+    id: Math.random(),
+    title: "Joker",
+    genre: "Fiction",
+    rating: 9.4,
+    description:
+      "It centers around two real-life Indian revolutionaries, Alluri Sitarama Raju (Charan) and Komaram Bheem (Rama Rao)",
+  },
+  {
+    id: Math.random(),
+    title: "Avatar",
+    genre: "Action",
+    rating: 9.9,
+    description:
+      "It centers around two real-life Indian revolutionaries, Alluri Sitarama Raju (Charan) and Komaram Bheem (Rama Rao)",
+  },
+  {
+    id: Math.random(),
+    title: "Broker",
+    genre: "Fiction",
+    rating: 7.2,
+    description:
+      "It centers around two real-life Indian revolutionaries, Alluri Sitarama Raju (Charan) and Komaram Bheem (Rama Rao)",
+  },
+];
+
+export default movies;
