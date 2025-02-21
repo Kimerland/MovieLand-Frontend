97b0598591932d0264b3e846f62cbee6704bbef5
Kirill Senchuk <kimerland.project@gmail.com>
2025-02-21 22:49:56 +0300

    WatchFilms styles + adaptive

--- src/components/CinemaContent/WatchFilms/WatchFilms.tsx
+++ src/components/CinemaContent/WatchFilms/WatchFilms.tsx
@@ -1,31 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ICinema } from "../CinemaContent";
+import WatchStyles from "../WatchFilms/WatchFilms.module.scss";
 
 const WatchFilms = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState<ICinema | null>(null);
-
+  const [videoId, setVideoId] = useState<string | null>(null);
   useEffect(() => {
-    fetch(`https://www.omdbapi.com/?i=${id}&apikey=1af1e3f2`)
-      .then((req) => req.json())
+    if (!id) return;
+    fetch(`http://localhost:5000/api/movies/info/${id}`)
+      .then((res) => res.json())
       .then((data) => setMovie(data));
   }, [id]);
 
+  useEffect(() => {
+    if (!movie?.Title) return;
+    fetch(
+      `http://localhost:5000/api/movies/trailer?title=${encodeURIComponent(
+        movie.Title
+      )}`
+    )
+      .then((res) => res.json())
+      .then((data) => setVideoId(data.videoId));
+  }, [movie?.Title]);
+
   return (
-    <div>
+    <main className={WatchStyles.wrapper_watch}>
       {movie ? (
         <>
-          <p>{movie.Title}</p>
-          <img src={movie.Poster} alt={movie.Title} />
-          <p>{movie.imdbRating}</p>
-          <p>{movie.Year}</p>
-          <p>{movie.Plot}</p>
+          <div className={WatchStyles.trailer_container}>
+            {videoId ? (
+              <iframe
+                src={`https://www.youtube.com/embed/${videoId}`}
+                title="YouTube video player"
+                allowFullScreen
+              ></iframe>
+            ) : (
+              <p>We don't have trailer</p>
+            )}
+          </div>
+
+          <div className={WatchStyles.movie_info}>
+            <div className={WatchStyles.raiting_container}>
+              <span className={WatchStyles.movie_watch}>
+                ⭐ {movie.imdbRating}
+              </span>
+              <p className={WatchStyles.year_watch}>{movie.Year}</p>
+            </div>
+
+            <p className={WatchStyles.title_watch}>{movie.Title}</p>
+            <p className={WatchStyles.plot_watch}>{movie.Plot}</p>
+          </div>
         </>
       ) : (
         <p>Loading...</p>
       )}
-    </div>
+    </main>
   );
 };
 
--- src/pages/WatchFilmsPage.tsx
+++ src/pages/WatchFilmsPage.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import StyleContent from "../components/HomeContent/HomeContent.module.scss";
 import WatchFilms from "../components/CinemaContent/WatchFilms/WatchFilms";
 import Header from "../components/Header/Header";
 
 const WatchFilmsPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 100);
+  }, []);
   return (
-    <>
-      <Header isSticky={false} />
-      <WatchFilms />
-    </>
+    <div className={StyleContent.home_wrapper}>
+      <div
+        className={`${StyleContent.home_page} ${
+          isVisible ? StyleContent.active : ""
+        }`}
+      >
+        <Header isSticky={false} />
+        <WatchFilms />
+      </div>
+    </div>
   );
 };
 
